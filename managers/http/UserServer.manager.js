8b56537cac4d4c20ba34fa484e8f204f01d4d3ec
PolaMalak <polamalak96@gmail.com>
2026-02-22 12:06:34 +0200

    refactor: make base url an env variable

--- managers/http/UserServer.manager.js
+++ managers/http/UserServer.manager.js
@@ -38,10 +38,11 @@ module.exports = class UserServer {
     /** a single middleware to handle all */
     app.all("/api/:moduleName/:fnName", this.userApi.mw);
 
+    const port = process.env.PORT || this.config.dotEnv.USER_PORT;
     let server = http.createServer(app);
-    server.listen(this.config.dotEnv.USER_PORT, () => {
+    server.listen(port, () => {
       console.log(
-        `${this.config.dotEnv.SERVICE_NAME.toUpperCase()} is running on port: ${this.config.dotEnv.USER_PORT}`,
+        `${this.config.dotEnv.SERVICE_NAME.toUpperCase()} is running on port: ${port}`,
       );
     });
   }
